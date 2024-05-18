ebe1b3130ee28efbb989f171a4a44f9e536f9863
Rodrigo Moreira <moreira.rodrigo@outlook.com.br>
2024-05-18 19:13:57 -0300

    criacao da aba prod

--- home.py
+++ home.py
@@ -2,94 +2,116 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.header('Dashboard Produção de Petróleo - IPEA')
+st.set_page_config(layout='wide')
 
-# BREVE TRATAMENTO DA BASE
-df_producao = pd.read_csv("bases\data_ext\df_producao_petroleo_T.csv", sep=";", decimal=".")
-df_producao["Data"] = pd.to_datetime(df_producao['Data'])
-df_producao = df_producao.set_index(df_producao['Data'])
-df_producao['Mes-Ano'] = df_producao['Data'].apply(lambda x: str(x.month) + '-' + str(x.year))
-df_producao['Mes'] = df_producao['Data'].dt.strftime('%B')
-df_producao['Ano'] = df_producao['Data'].dt.year
-df_paises = df_producao.drop(["Data", "Ano", "Mes", "Mes-Ano"], axis = 1 ).columns
+st.header('Produção de Petróleo - IPEA')
+st.subheader('Produção Mundial em Barris dos Ultimos 20 Anos.')
+
+# BREVE TRATAMENTO DA BASE GERAL
+df_producao = pd.read_csv("bases\data_ext\df_producao_petroleo_dash.csv", sep=";", decimal=".")
+df_producao = df_producao.loc[df_producao['Continent'] != 'World']
+df_producao["Mês"] = pd.to_datetime(df_producao['Mês'])
+df_producao = df_producao.set_index(df_producao['Mês'])
+df_producao['Mes'] = df_producao['Mês'].dt.strftime('%B')
+df_producao['Ano'] = df_producao['Mês'].dt.year
+df_producao = df_producao[df_producao['Ano'] >= 2004]
+
+# CRIACAO DO DF MUNDIAL
+df_agrupado_cont = df_producao.groupby(['Ano', 'Continent'])['Quantidade'].sum().reset_index()
+
+# DIVISAO DOS 2 SETORES DA METADE DE CIMA DA PAGINA DO STREAMLIT
+col1, col2 = st.columns(2)
+fig_ano = px.bar(df_agrupado_cont, x='Ano', y='Quantidade', color='Continent', 
+                color_discrete_map={
+                "Europe": "yellow",
+                "Asia": "violet",
+                "North America": "lightblue",
+                "South America": "lightgreen",
+                "Oceania": "grey",
+                "Africa": "goldenrod"},title="Produção de petróleo mundial")
+col1.plotly_chart(fig_ano, use_container_width=True)
+
+fig_conti = px.line(df_agrupado_cont, x="Ano", y="Quantidade", color='Continent', markers=True,
+                color_discrete_map={
+                "Europe": "yellow",
+                "Asia": "violet",
+                "North America": "lightblue",
+                "South America": "lightgreen",
+                "Oceania": "grey",
+                "Africa": "goldenrod"}, title="Produção de petróleo mundial por continente")
+col2.plotly_chart(fig_conti, use_container_width=True)
 
 
-# FILTRO LATERAL DE ANO E PAISES
+# DIVISAO DOS 3 SETORES DA METADE DE BAIXO DA PAGINA DO STREAMLIT
+st.markdown("<hr>", unsafe_allow_html=True)
 
+st.subheader('Produção Regional dos Ultimos 20 Anos.')
 
+col3, col4 = st.columns(2)
+
+# FILTRO LATERAL DE ANO, CONTINENTE E PAISES
 st.sidebar.subheader('Filtros:')
 fAno = st.sidebar.multiselect(
         "Selecione um ou mais Anos:", 
-        options=df_producao['Ano'].unique(),
-        default=df_producao['Ano'].unique()
+        options=df_producao['Ano'].sort_values().unique()
+        #,default=2023
+    )
+
+fContinentes = st.sidebar.multiselect(
+        'Selecione um ou mais continentes:',
+        options=df_producao['Continent'].unique()
+        #,default=df_producao['Continent'].unique()
     )
 
 fPaises = st.sidebar.multiselect(
         'Selecione um ou mais paises:',
-        options=df_paises
+        options=df_producao['Country'].sort_values().unique()
+        #,default='Brazil'
     )
 
-df_filtered = df_producao.loc[
-        df_producao['Data'].dt.year.isin(fAno)]
-
-df_filtered = df_producao.query(
-    "Ano == @fAno"
-)
+filtered_df = df_producao[
+    (df_producao['Ano'].isin(fAno) | (len(fAno) == 0)) &
+    (df_producao['Continent'].isin(fContinentes) | (len(fContinentes) == 0)) &
+    (df_producao['Country'].isin(fPaises) | (len(fPaises) == 0))]
 
 
-st.dataframe(df_filtered)
+filtered_df2 = filtered_df.groupby(['Ano', 'Continent', 'Country'])['Quantidade'].sum().reset_index()
 
 
 
-#pd.melt(df_prod.reset_index(), id_vars=['produto'], var_name='ano', value_name='valor').sort_values(['produto'], ascending=False)
+# GRAFICOS DA SEGUNDA METADE DA PAGINA
+df_agrupado_g = filtered_df2.groupby(filtered_df2['Country'])['Quantidade'].sum().reset_index()
+df_agrupado_g = df_agrupado_g.sort_values('Quantidade',ascending=False)
+df_top10 = df_agrupado_g.head(10)
+
+fig_ranking = px.bar(df_top10, x='Quantidade', y='Country', title="Ranking de Produção de Petróleo Mundial",orientation='h')
+col3.plotly_chart(fig_ranking, use_container_width=True)
+
+
+if fContinentes:
+    fig_pie_conti = px.pie(filtered_df2, names="Country", values="Quantidade",title="Produção de Petróleo Total do Continente Filtrado")
+    col4.plotly_chart(fig_pie_conti, use_container_width=True)
+else:
+    fig_pie_conti = px.pie(filtered_df2, names="Continent", values="Quantidade", color='Continent',
+                color_discrete_map={
+                "Europe": "yellow",
+                "Asia": "violet",
+                "North America": "lightblue",
+                "South America": "lightgreen",
+                "Oceania": "grey",
+                "Africa": "goldenrod"},title="Produção de Petróleo Total por Continente")
+    col4.plotly_chart(fig_pie_conti, use_container_width=True)
 
 
 
+col5, col6 = st.columns(2)
+
+fig_conti_reg = px.line(filtered_df2, x="Ano", y="Quantidade", color='Country', markers=True, title="Produção de Petróleo Anual por Pais")
+col5.plotly_chart(fig_conti_reg, use_container_width=True)
+
+st.markdown("<hr>", unsafe_allow_html=True)
 
 
-
-
-
-
-
-
-
-
-
-
-
-#ano_selecionado = st.sidebar.multiselect('Selecione um ou mais anos:', df_producao['Ano'].unique())
-#df_f_ano = df_producao[df_producao['Data'].dt.year.isin(ano_selecionado)]
-
-
-#colunas_selecionada = st.sidebar.multiselect('Selecione um ou mais paises:', df_paises)
-#df_f_cols = df_producao.columns.isin(df_paises)
-
-#df_producao
-
-
-
-#col1, col2 = st.columns(2)
-#col3, col4, col5 = st.columns(3)
-
-#fig_date = px.bar(df_filtered, x=df_filtered.index, y=df_filtered.columns, title="Produção de petroleo Mundial")
-#col1.plotly_chart(fig_date)
-
-
-
-
-
-
-
-
-
-
-
-#if st.sidebar.checkbox('Mostre o Dataframe'):
-#    df_filtered
-
-
-
-
-
-
+# SWITCH DE EXIBICAO DO DF
+if st.checkbox('Mostre os Dados'):
+    filtered_df
--- pages/2_teste.py
+++ pages/2_teste.py
@@ -1,36 +1,26 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+import streamlit as st
 
-st.title('Uber pickups in NYC')
+# Exemplo de dados fictícios
+data = {
+    'Animal Type': ['Dog', 'Cat', 'Spider'],
+    'Breed': ['bulldog', 'asky', 'asky'],
+    'Color': ['brown', 'yellow', 'black']
+}
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+df = pd.DataFrame(data)
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+# Filtros multiselect independentes
+options1 = st.multiselect('Animal Type', df['Animal Type'].unique())
+options2 = st.multiselect('Breed', df['Breed'].unique())
+options3 = st.multiselect('Color', df['Color'].unique())
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+# Filtrando o DataFrame com base nas seleções
+filtered_df = df[
+    (df['Animal Type'].isin(options1) | (len(options1) == 0)) &
+    (df['Breed'].isin(options2) | (len(options2) == 0)) &
+    (df['Color'].isin(options3) | (len(options3) == 0))
+]
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+# Exibindo o DataFrame filtrado
+st.write(filtered_df)
