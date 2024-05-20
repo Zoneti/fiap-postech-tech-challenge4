8b12a84bf52f91700eae391725678c9992fe7f63
Gabriel Zoneti <gabriel.zoneti@gmail.com>
2024-05-20 17:28:10 -0300

    add: teste Botão Projetar

--- pages/3_Projeções.py
+++ pages/3_Projeções.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import pickle
+from prophet import Prophet
 
 st.set_page_config(layout='wide')
 
@@ -29,3 +31,8 @@ input_tempo_experiencia
 col1, col2 = st.columns(2)
 fig_preco = px.line(filtered_df, x="Data", y="Preco",  title="Preço do petróleo")
 col1.plotly_chart(fig_preco, use_container_width=True)
+
+#Predições 
+if st.button('Projetar'):
+    with open('../prophet_model.pkl', 'rb') as f:
+        prophet_model = pickle.load(f)
